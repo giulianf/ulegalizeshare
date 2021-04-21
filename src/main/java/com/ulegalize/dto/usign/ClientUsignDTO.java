5a815a9b8cc322d70f98e7e36bcff5d1944f7684
fumajul <julien.fumanti@finauxa.com>
2021-04-21 16:45:59 +0200

    1.0.9 client usign dto added

---
+++ src/main/java/com/ulegalize/dto/usign/ClientUsignDTO.java
@@ -0,0 +1,26 @@
+package com.ulegalize.dto.usign;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class ClientUsignDTO implements Serializable {
+    public ClientUsignDTO() {
+    }
+
+    private Long id;
+    private String fullName;
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private String email;
+    private int left = 0;
+    private int top = 0;
+    private int width = 160;
+    private int height = 80;
+
+    private Date birthdate;
+
+}
