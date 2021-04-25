f675e8108303ebc892ddb29062f73e01cbfd74b7
fumajul <julien.fumanti@finauxa.com>
2021-04-25 17:38:16 +0200

    version 1.1.5 enum mail template

--- src/main/java/com/ulegalize/mail/transparency/EnumMailTemplate.java
+++ src/main/java/com/ulegalize/mail/transparency/EnumMailTemplate.java
@@ -1,6 +1,8 @@
 package com.ulegalize.mail.transparency;
 
 public enum EnumMailTemplate {
+
+// transparency
     INVITELITIGANTCONSEIL("mailInviteLitigant", "Invitation sur votre dossier", "Invitation on your folder", "Uitnodiging om aan een bestand"),
     INVITECONSEIL("mailInviteConseil", "Invitation à travailler sur un dossier", "Invitation to work on a folder", "Uitnodiging om aan een bestand te werken"),
     MAILBLANKTEMPLATE("mailBlankTemplate", "Partage de votre dossier", "Sharing your folder", "Sharing your folder"),
@@ -10,7 +12,16 @@ public enum EnumMailTemplate {
     MAILATTACHDOCUMENTCASE("mailAttachDocumentCase", "Signature du document ", "Document signature", "Documenthandtekening"),
     MAILSAVEOWNERCASE("mailSaveOwnerCase", "Changement de la prise en charge pour le cas juridique ", "Change of coverage for the legal case", "Een kabinet toevoegen voor de case"),
     MAILUPDATEDOCUMENTCASE("mailUpdateDocumentCase", "Modification du cas juridique ", "Modification of the legal case", "Wijziging van de rechtszaak"),
-    MAILFINISHSIGNDOCUMENT("mailFinishSignDocument", "Signature du document ", "Document signature", "Documenthandtekening");
+    MAILFINISHSIGNDOCUMENT("mailFinishSignDocument", "Signature du document ", "Document signature", "Documenthandtekening"),
+
+    // lawfirm
+    MAILAPPOINTMENT_ADDED_NOTIFICATION("mailAppointmentAddedNotification", "Ulegalize: Notification agenda", "Ulegalize: Notification agenda", "Ulegalize: Notificatie agenda"),
+    MAILAPPOINTMENT_CANCEL_NOTIFICATION("mailAppointmentCancelNotification", "Ulegalize: Annulation agenda", "Ulegalize: Cancel agenda", "Ulegalize: Cancel agenda"),
+    MAILAPPOINTMENTCONFIRMEDTEMPLATE("mailAppointmentConfirmedTemplate", "Ulegalize: Demande de rendez-vous confirmée", "Ulegalize: Appointment request confirmed", "Ulegalize: Afspraakverzoek bevestigd"),
+    MAILAPPOINTMENTREGISTEREDTEMPLATE("mailAppointmentRegisteredTemplate", "Nouvelle demande de rendez-vous", "New appointment request", "Nieuw afspraakverzoek"),
+    MAILNEWAPPOINTMENTREQUESTTEMPLATE("mailNewAppointmentRequestTemplate", "Demande de rendez-vous enregistrée", "Appointment request recorded", "Afspraakverzoek geregistreerd"),
+    MAILSHAREDFOLDERUSERTEMPLATE("mailSharedFolderUserTemplate", "Nouveau partage de dossier ", "New folder sharing ", "Nieuwe mappen delen "),
+    MAILSHAREDUSERSECURITYTEMPLATE("mailSharedUserSecurityTemplate", "Nouvel acces au cabinet ", "New access to the practice ", "Nieuwe toegang tot de praktijk ");
 
     private String name;
     private String subjectFr;
