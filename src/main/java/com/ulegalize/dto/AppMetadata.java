8d7fbd0085b174153daeb71bbfe340abe57bb3a4
fumajul <julien.fumanti@finauxa.com>
2021-06-20 13:18:20 +0200

    version 1.1.24 signedup_submitted_portal

--- src/main/java/com/ulegalize/dto/AppMetadata.java
+++ src/main/java/com/ulegalize/dto/AppMetadata.java
@@ -1,13 +1,15 @@
 package com.ulegalize.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 public class AppMetadata {
-    public AppMetadata(){}
-
+    public AppMetadata() {
+    }
 
     @Getter
     @Setter
     private Boolean signedup_submitted;
+    @Getter
+    @Setter
+    private Boolean signedup_submitted_portal;
 }
