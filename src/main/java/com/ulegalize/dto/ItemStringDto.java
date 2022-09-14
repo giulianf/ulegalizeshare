b802d665e8603216ae6a18b30598131dc8eaae95
younesarifi <younes.arifi@finauxa.com>
2022-09-14 14:31:00 +0200

    Update version 3.3.4 to 3.3.5

--- src/main/java/com/ulegalize/dto/ItemStringDto.java
+++ src/main/java/com/ulegalize/dto/ItemStringDto.java
@@ -1,22 +1,12 @@
 package com.ulegalize.dto;
 
-import lombok.Getter;
-
 public class ItemStringDto extends Item<String> {
-
-    @Getter
-    private Long extraId;
-
     public ItemStringDto() {
         super("", "");
     }
 
+
     public ItemStringDto(String value, String label) {
         super(value, label);
-    }
-
-    public ItemStringDto(String value, String label, Long extraId) {
-        super(value, label);
-        this.extraId = extraId;
     }
 }
