31674ffce8df8fb32cad9574f44fe9c3f875f83a
fumajul <julien.fumanti@finauxa.com>
2023-03-30 10:18:00 +0200

    version 3.21.1 dossier_id

--- src/main/java/com/ulegalize/dto/report/EtatReportDTO.java
+++ src/main/java/com/ulegalize/dto/report/EtatReportDTO.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class EtatReportDTO {
+    private Long dossierId;
 
     private List<InvoiceDTO> invoiceList;
     private List<ComptaDTO> tiersAffaireList;
