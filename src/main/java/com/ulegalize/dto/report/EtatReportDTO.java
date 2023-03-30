238ff044d25142ae08394ec73ef0ec4b86fb2561
fumajul <julien.fumanti@finauxa.com>
2023-03-30 19:01:25 +0200

    version 3.21.3 invoice

--- src/main/java/com/ulegalize/dto/report/EtatReportDTO.java
+++ src/main/java/com/ulegalize/dto/report/EtatReportDTO.java
@@ -2,7 +2,7 @@ package com.ulegalize.dto.report;
 
 import com.ulegalize.dto.ComptaDTO;
 import com.ulegalize.dto.InvoiceDTO;
-import com.ulegalize.dto.template.CompleteDossierDTO;
+import com.ulegalize.dto.template.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,11 +15,27 @@ import java.util.List;
 @NoArgsConstructor
 public class EtatReportDTO {
     private Long dossierId;
-
+    private CompleteDossierDTO completeDossierDTO;
     private List<InvoiceDTO> invoiceList;
     private List<ComptaDTO> tiersAffaireList;
     private List<ComptaDTO> honoraireAffaireList;
-    private CompleteDossierDTO completeDossierDTO;
+
+    /**
+     * The Item prestation.
+     */
+    private List<ItemPrestation> itemPrestation;
+    /**
+     * The Item debours.
+     */
+    private List<ItemDebours> itemDebours;
+    /**
+     * The Item frais admins.
+     */
+    private List<ItemFraisAdmin> itemFraisAdmins;
+    /**
+     * The Item frais collas.
+     */
+    private List<ItemFraisColla> itemFraisCollas;
 
     /**
      * @param completeDossierDTO complete dossier
