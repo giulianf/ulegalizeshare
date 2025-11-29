01c047eb14f6b2f60de2aeb2a87bb574b4265ec7
fumajul <julien.fumanti@finauxa.com>
2025-11-29 12:50:10 +0100

    6.50.4

--- src/main/java/com/ulegalize/dto/ItemStringDto.java
+++ src/main/java/com/ulegalize/dto/ItemStringDto.java
@@ -3,30 +3,50 @@ package com.ulegalize.dto;
 import java.io.Serial;
 import java.io.Serializable;
 
+import lombok.Getter;
+
 /**
  * The type Item string dto.
  */
 public class ItemStringDto extends Item<String> implements Serializable {
-    /**
-     * serial
-     */
-    @Serial
-    private static final long serialVersionUID = 1L;
-    /**
-     * Instantiates a new Item string dto.
-     */
-    public ItemStringDto() {
-        super("", "");
-    }
+  /**
+   * serial
+   */
+  @Serial
+  private static final long serialVersionUID = 1L;
+  /**
+   * The Is default.
+   */
+  @Getter
+  private String isDefault;
+
+  /**
+   * Instantiates a new Item string dto.
+   */
+  public ItemStringDto() {
+    super("", "");
+  }
 
 
-    /**
-     * Instantiates a new Item string dto.
-     *
-     * @param value the value
-     * @param label the label
-     */
-    public ItemStringDto(String value, String label) {
-        super(value, label);
-    }
+  /**
+   * Instantiates a new Item string dto.
+   *
+   * @param value the value
+   * @param label the label
+   */
+  public ItemStringDto(String value, String label) {
+    super(value, label);
+  }
+  /**
+   * Instantiates a new Item String dto.
+   *
+   * @param value the value
+   * @param label the label
+   * @param extra the extra
+   */
+  public ItemStringDto(String value, String label, String extra) {
+    super(value, label);
+
+    this.isDefault = extra;
+  }
 }
