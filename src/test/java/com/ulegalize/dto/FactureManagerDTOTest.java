3b8dce6fca1f1337f96f8e352304cdaa69ddd938
fumajul <julien.fumanti@finauxa.com>
2026-02-06 21:55:38 +0100

    7.3.4

--- src/test/java/com/ulegalize/dto/FactureManagerDTOTest.java
+++ src/test/java/com/ulegalize/dto/FactureManagerDTOTest.java
@@ -21,8 +21,9 @@ class FactureManagerDTOTest {
         Long factureId = 456L;
         Long id = 789L;
         String avatarUrl = "avatarUrl";
+        String initialesGestionnaire = "jt";
 
-        FactureManagerDTO dto = new FactureManagerDTO(userId, label, factureId, id, avatarUrl, 1);
+        FactureManagerDTO dto = new FactureManagerDTO(userId, label, factureId, id, avatarUrl, 1, initialesGestionnaire);
         assertEquals(userId, dto.getValue());
         assertEquals(factureId, dto.getFactureId());
         assertEquals(id, dto.getId());
