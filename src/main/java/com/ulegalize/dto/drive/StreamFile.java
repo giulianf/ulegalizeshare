9d3ac6f18f200522e77e2dd51b385c4921e993d9
fumajul <julien.fumanti@finauxa.com>
2026-03-04 19:24:12 +0100

    7.5.4

--- src/main/java/com/ulegalize/dto/drive/StreamFile.java
+++ src/main/java/com/ulegalize/dto/drive/StreamFile.java
@@ -1,6 +1,12 @@
 package com.ulegalize.dto.drive;
 
 import java.io.InputStream;
+import java.text.Normalizer;
 
-public record StreamFile(InputStream inputStream, String filename, boolean empty) {}
+public record StreamFile(InputStream inputStream, String filename, boolean empty) {
+
+  public String normalizedFilename() {
+    return Normalizer.normalize(filename, Normalizer.Form.NFC);
+  }
+}
 
