92d079bd7df1211d92e42ce4c3720345582998ac
fumajul <julien.fumanti@finauxa.com>
2026-05-28 21:16:51 +0200

    7.12.1

--- src/main/java/com/ulegalize/dto/DossierAiMetadataDTO.java
+++ src/main/java/com/ulegalize/dto/DossierAiMetadataDTO.java
@@ -1,10 +1,12 @@
 package com.ulegalize.dto;
 
 import lombok.Data;
+
+import java.io.Serializable;
 import java.util.List;
 
 @Data
-public class DossierAiMetadataDTO {
+public class DossierAiMetadataDTO implements Serializable {
 
     private List<String> warnings;
 
